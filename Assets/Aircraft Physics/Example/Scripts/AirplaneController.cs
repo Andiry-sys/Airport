b2956cab6f8cda9568e3f242cf7a6e7036cacfb0
Andiry-sys <marchycandriy@gmail.com>
2022-11-11 14:45:46 +0200

    Add project files.

---
+++ Assets/Aircraft Physics/Example/Scripts/AirplaneController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AirplaneController : MonoBehaviour
+{
+    [SerializeField]
+    private float rollControlSensitivity = 0.2f;
+    [SerializeField]
+    private float pitchControlSensitivity = 0.2f;
+    [SerializeField]
+    private float yawControlSensitivity = 0.2f;
+    [SerializeField]
+    private float thrustControlSensitivity = 0.01f;
+    [SerializeField]
+    private float flapControlSensitivity = 0.15f;
+
+
+    private float pitch;
+    private float yaw;
+    private float roll;
+    private float flap;
+
+    private float thrustPercent;
+    private bool brake = false;
+
+    private AircraftPhysics aircraftPhysics;
+    private Rotator propeller;
+
+    private void Start()
+    {
+        aircraftPhysics = GetComponent<AircraftPhysics>();
+        propeller = FindObjectOfType<Rotator>();
+        SetThrust(0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SetThrust(thrustPercent + thrustControlSensitivity);
+        }
+        propeller.speed = thrustPercent * 1500f;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            thrustControlSensitivity *= -1;
+            flapControlSensitivity *= -1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            brake = !brake;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            flap += flapControlSensitivity;
+            //clamp
+            flap = Mathf.Clamp(flap,0f,Mathf.Deg2Rad * 40);
+        }
+
+        pitch = pitchControlSensitivity * Input.GetAxis("Vertical");
+        roll = rollControlSensitivity * Input.GetAxis("Horizontal");
+        yaw = yawControlSensitivity * Input.GetAxis("Yaw");
+    }
+
+    private void SetThrust(float percent)
+    {
+        thrustPercent = Mathf.Clamp01(percent);
+    }
+
+    private void FixedUpdate()
+    {
+        aircraftPhysics.SetControlSurfecesAngles(pitch,roll,yaw,flap);
+        aircraftPhysics.SetThrustPercent(thrustPercent);
+        aircraftPhysics.Brake(brake);
+    }
+}
